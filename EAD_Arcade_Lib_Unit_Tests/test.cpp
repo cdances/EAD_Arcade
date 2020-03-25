4cd58c9fc1eab566ae5a4a773651b41b98ef2e68
Chris Dances <chris.a.dances@gmail.com>
2020-03-25 14:23:09 -0400

    Added more tests to achieve full code coverage

--- EAD_Arcade_Lib_Unit_Tests/test.cpp
+++ EAD_Arcade_Lib_Unit_Tests/test.cpp
@@ -3,17 +3,31 @@
 #include "MemoryGame.h"
 TEST(WhackAMoleTests, callWhackAMole) {
     Game * gameTest = new WhackAMole();
+    delete gameTest;
 }
+
 TEST(WhackAMoleTests, checkWhackAMoleType) {
     Game * gameTest = new WhackAMole();
     ASSERT_STREQ("Whack-A-Mole", gameTest->getType());
 }
 
+TEST(WhackAMoleTests, checkWhackAMolePlay) {
+    Game* gameTest = new WhackAMole();
+    gameTest->play();
+}
+
 TEST(MemoryGameTests, callMemoryGame) {
     Game * gameTest = new MemoryGame();
+    delete gameTest;
 }
 
 TEST(MemoryGameTests, checkMemoryGameType) {
     Game * gameTest = new MemoryGame();
     ASSERT_STREQ("Memory Game", gameTest->getType());
+}
+
+
+TEST(MemoryGameTests, checkMemoryGamePlay) {
+    Game* gameTest = new MemoryGame();
+    gameTest->play();
 }
